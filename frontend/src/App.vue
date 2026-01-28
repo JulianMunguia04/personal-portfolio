50543a6b7477c19550c40d2ebed59c86a414848d
JulianMunguia04 <julianmunguia04@gmail.com>
2026-01-28 12:27:03 -0700

    Initial Folder Setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/projects">Projects</router-link> |
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+
+  <router-view />
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/Projects.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to my portfolio!</p>
+  </div>
+</template>
+
+<script setup>
+// Nothing yet
+</script>
