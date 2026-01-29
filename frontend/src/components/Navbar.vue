01acb67edcb1ccee878821894e4bb312489553ca
JulianMunguia04 <julianmunguia04@gmail.com>
2026-01-29 16:12:04 -0700

    Navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,14 +3,38 @@
     <router-link class="router-link" to="/">
       <i class="pi pi-home nav-icons"></i>
 
-      <!-- Tooltip / hint -->
       <div class="hint-box">
         Home
       </div>
     </router-link>
-    <router-link class="router-link" to="/about">About</router-link>
-    <router-link class="router-link" to="/projects">Projects</router-link>
-    <router-link class="router-link" to="/contact">Contact</router-link>
+    <router-link class="router-link" to="/projects">
+      <i class="pi pi-folder nav-icons"></i>
+
+      <div class="hint-box">
+        Projects
+      </div>
+    </router-link>
+    <router-link class="router-link" to="/technologies">
+      <i class="pi pi-cog nav-icons"></i>
+
+      <div class="hint-box">
+        Technologies
+      </div>
+    </router-link>
+    <router-link class="router-link" to="/experience">
+      <i class="pi pi-building nav-icons"></i>
+
+      <div class="hint-box">
+        Experience
+      </div>
+    </router-link>
+    <router-link class="router-link" to="/contact">
+      <i class="pi pi-send nav-icons"></i>
+
+      <div class="hint-box">
+        Contact
+      </div>
+    </router-link>
   </nav>
 
   <main class="nav-margin">
@@ -39,8 +63,10 @@ nav {
   top: 0;
   left: 50%;
   transform: translate(-50%, -100%);
-  width: 50%;
-  height: 5vh;
+  width: auto;
+  padding-left: 5%;
+  padding-right: 5%;
+  height: 8vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -51,6 +77,7 @@ nav {
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
   opacity: 0;
   transition: all 0.6s ease;
+  margin-top: 1vh;
 }
 
 .show {
@@ -59,7 +86,7 @@ nav {
 }
 
 .nav-margin {
-  margin-top: 5vh;
+  margin-top: 8vh;
 }
 
 .router-link {
@@ -71,10 +98,11 @@ nav {
   font-size: 1rem;
   position: relative;
 
-  width: 4vh; 
-  height: 4vh;   
+  width: 6.5vh; 
+  height: 6.5vh;   
   border-radius: 50%;
   transition-duration: 0.15s;
+  border: 1px solid rgba(150, 150, 150, 1);;
 }
 
 .nav-icons {
@@ -84,12 +112,12 @@ nav {
 }
 .router-link:hover{
   background-color: #3f3f3fff;
-  transform: scale(1.1);
+  transform: scale(1.3) translateY(10%);
 }
 
 .hint-box {
-  position: absolute;  /* <-- relative to parent */
-  top: 100%;           /* below the parent */
+  position: absolute;
+  top: 100%;
   left: 50%;
   transform: translateX(-50%);
   background-color: #3f3f3fff;
@@ -119,11 +147,14 @@ nav {
   visibility: hidden; 
   transition: opacity 0.3s ease, transform 0.3s ease;
   transform: translateX(-50%) translateY(5px);
+  transition-delay: 0.15s; 
 }
 
 .router-link:hover .hint-box {
   opacity: 1;
   visibility: visible;
   transform: translateX(-50%) translateY(0);
+  transition-delay: 0s; 
+  border: 1px solid rgb(204, 204, 204);
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,16 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Projects from '../views/Projects.vue';
 import Contact from '../views/Contact.vue';
+import Technologies from '../views/Technologies.vue';
+import Experience from '../views/Experience.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
   { path: '/projects', name: 'Projects', component: Projects },
   { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/technologies', name: 'Technologies', component: Technologies },
+  { path: '/experience', name: 'Experience', component: Experience},
 ];
 
 const router = createRouter({
