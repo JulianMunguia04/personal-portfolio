b4fa63a0f5c2c7ee235209858f0f606e0c8dc968
JulianMunguia04 <julianmunguia04@gmail.com>
2026-01-29 11:48:54 -0700

    Background and animations

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,33 @@
 <script setup>
   import { useRoute } from 'vue-router'
-  import Navbar from './components/Navbar.vue'
+  import Navbar from '@/components/Navbar.vue'
 
   const route = useRoute()
 </script>
 
 <template>
   <Navbar/>
+  <div class="video-bg">
+    <video autoplay muted loop playsinline>
+      <source src="/background.mp4" type="video/mp4" />
+    </video>
+  </div>
 
   <router-view />
 </template>
 
-<style scoped>
-  div{
-    height: 200vh;
-  }
+<style>
+.video-bg {
+  position: fixed;
+  inset: 0;
+  z-index: -1;
+  overflow: hidden;
+  opacity: 40%;
+}
+
+.video-bg video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    <h1>Home Page</h1>
-    <p>Welcome to my portfolio!</p>
-  </div>
+  <main :class="{ show: loaded }">
+    Welcome
+  </main>
 </template>
 
 <script setup>
-// Nothing yet
+  import { ref, onMounted } from 'vue'
+
+  const loaded = ref(false)
+  const showWaitTime = 1400
+
+  onMounted(() => {
+    setTimeout(() => {
+      loaded.value = true
+    }, showWaitTime)
+  })
 </script>
 
 <style scoped>
+  main{
+    opacity: 0;
+    transition: all 0.6s ease;
+  }
+  .show {
+    transform: translateY(0);
+    opacity: 1;
+  }
 </style>
